4948aad9f6e4e6eb1e87a0770c023fe4e30912e3
Jason <jasonnnnli1997@gmail.com>
2020-07-28 12:50:51 -0400

    Initial commit

---
+++ app/src/main/java/edu/tacoma/uw/jasonli7/team12project/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.tacoma.uw.jasonli7.team12project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
